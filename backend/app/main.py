49ff200cad24f5ecee3bc32c496b696bc5030f95
Pruebas <sindicuealvarado@hotmail.com>
2026-03-05 12:40:35 -0500

    x3

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import health, catalog, purchases, auth
+from .routers import health, catalog, purchases, auth, admin_tracks
 
 app = FastAPI(title="Chinook Store API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # puedes limitar a tu IP del front si quieres
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# opcional sin versión
 app.include_router(health.router)
+
+# versionado
 app.include_router(health.router, prefix="/v1")
+app.include_router(auth.router, prefix="/v1")
 app.include_router(catalog.router, prefix="/v1")
 app.include_router(purchases.router, prefix="/v1")
-app.include_router(auth.router, prefix="/v1")
-app.include_router(catalog.router, prefix="/v1")      # público
-app.include_router(purchases.router, prefix="/v1")    # requiere token
-app.include_router(admin_tracks.router, prefix="/v1") # requiere admin
+app.include_router(admin_tracks.router, prefix="/v1")
